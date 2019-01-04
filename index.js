0f9d0d29818261d3b7e1446b26bb936e4d293705
Edward Whelan <vroxing@gmail.com>
2019-01-04 10:09:37 -0500

    set up db sessions packages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //ENV VARIABLES
 require('dotenv').config();
 
+//database
+const db = require('./models/db');
 
 // import packages
 const bodyParser = require('body-parser');
@@ -10,12 +12,32 @@ const morgan = require('morgan');
 
 //initialize app
 const app = express();
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+
+app.use(session({
+  store: new pgSession({
+    pgPromise: db
+  }),
+  secret: 'g8j3lsa0sk2bbfhv5486nfgvge-387356!', // remember to adjust before deploying
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000 //Adjusts max time of session to 30 days
+  }
+}));
 
 // app.use(express.static(path.join(__dirname, 'public'))); // all static files will be served from public folder
 app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('combined'));
+
+app.use((req, res, next) => {
+  let isLoggedIn = req.session.user ? true : false;
+  console.log(isLoggedIn);
+  next();
+})
+
 
 request = [
   {
