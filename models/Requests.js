ef72a51e1af9923d73bf0cbc1c56743fbb07a9bb
Edward Whelan <vroxing@gmail.com>
2019-01-05 14:05:57 -0500

    add create method to REQUESTS

--- models/Requests.js
+++ models/Requests.js
@@ -10,17 +10,38 @@ class Requests {
       this.USER_ID_TO = USER_ID_TO,
       this.is_private = is_private
   }
+
+  // CREATE ===============================================
+  // make a new request
+  static addRequest(USER_ID_FROM, title, REQUEST_contents, USER_ID_TO, is_private) {
+    return db.one(`
+  insert into REQUESTS
+  (USER_ID_FROM, title, REQUEST_contents, USER_ID_TO, is_private)
+  values
+  ($1, $2, $3, $4, $5)
+  returning id
+  `, [USER_ID_FROM, title, REQUEST_contents, USER_ID_TO, is_private])
+      .then(data => {
+        const u = new Requests(data.id, USER_ID_FROM, title, REQUEST_contents, USER_ID_TO, is_private)
+        return u
+      })
+
+  }
+  // RETRIEVE ===============================================
+  // get all public requests
   static getPublicRequests() {
     return db.any(`
-    select *
-    from REQUESTS
-    where is_private=false
-    `)
+  select *
+  from REQUESTS
+  where is_private=false
+  `)
       .then(r => {
         return r
       })
   }
 
+  // UPDATE ===============================================
+  // DELETE ===============================================
 
 };
 
