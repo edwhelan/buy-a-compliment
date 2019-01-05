55b8d47f1ff436b5183b17892bc8ec9c5aa9ca39
Edward Whelan <vroxing@gmail.com>
2019-01-05 15:12:57 -0500

    add register component. move login to correct folder

---
+++ client/src/components/Register.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Register = (props) => {
+  return (
+    <form action="/register" method="POST">
+      <label>
+        Email address:<br />
+        <input type="email" name="email" required />
+      </label>
+      <br />
+      <label>
+        Display name:<br />
+        <input type="text" name="displayName" required />
+      </label>
+      <br />
+      <label>
+        Password:<br />
+        <input type="password" name="password" required />
+      </label>
+      <br />
+      <input type="submit" value="Register" />
+    </form >
+  )
+}
+
+export default Register;
