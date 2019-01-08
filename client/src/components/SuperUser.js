56941ae40f8e8d8625a98124dc01a24288024852
Edward Whelan <vroxing@gmail.com>
2019-01-08 14:57:16 -0500

    set up form with react state for replying to requests

--- client/src/components/SuperUser.js
+++ client/src/components/SuperUser.js
@@ -6,11 +6,13 @@ class SuperUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: []
+      list: [],
+      replyBody: '',
+      requestIdToReplyTo: 0
     }
   }
 
-  //fetch requests of USER_ID_TO of currently logged in user
+  //fetch requests of USER_ID_TO of currently logged in user if they havent been responded to
   async componentDidMount() {
     await fetch('/api/requestsToUser')
       .catch(err => {
@@ -28,11 +30,29 @@ class SuperUser extends Component {
   }
   //display below
   //each of these should have the ability to be responded to
-  // col has_responded?
   render() {
     return (
       <>
-        <div>Reply to specific request</div>
+        <div>Reply to specific request
+        <form onSubmit={this._onSubmit}>
+            <label>
+              Reply:
+              <textarea value={this.state.replyBody} onChange={this._onChangeReply} name="reply_body" form="usrform" required></textarea>
+            </label>
+            <br />
+            <input type='submit' />
+          </form>
+          <div>Replying to:
+          <select name="idToReplyTo" onChange={this._onChangeRequestID}>
+              <option value="" selected disabled hidden>Choose here</option>
+              {this.state.list.map(item => {
+                return (<option value={item.id}>{item.title}</option>)
+              })}
+            </select>
+            {this.state.list.map(box => {
+
+            })}</div>
+        </div>
         <div>here are your current pending requests to reply to</div>
         <div className='replies request-wrapper'>
           {
@@ -44,6 +64,18 @@ class SuperUser extends Component {
         </div>
       </>
     )
+  }
+  _onChangeRequestID = (event) => {
+    this.setState({
+      requestIdToReplyTo: event.target.value
+    })
+    console.log(event.target.value)
+  }
+
+  _onChangeReply = (event) => {
+    this.setState({
+      replyBody: event.target.value
+    })
   }
 }
 
