f64edb55a5ad100f9d56374ea17720ccb554078a
Edward Whelan <vroxing@gmail.com>
2019-01-11 17:43:21 -0500

    add styles to completedRequests cards

--- client/src/components/CompletedRequest.js
+++ client/src/components/CompletedRequest.js
@@ -3,7 +3,9 @@ import React from 'react'
 const CompletedRequest = (props) => {
   return (
     <div className='completed-request' key={props.data.id}>
-      <p>{props.data.sender_name}: {props.data.title}</p>
+      <span className='blueText'>
+        <p>{props.data.sender_name}: {props.data.title}</p>
+      </span>
       <p>{props.data.request_contents}</p>
       <p>{props.data.recipient_name}: {props.data.reply}</p>
     </div>
